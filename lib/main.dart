27eba19dda2168acb6ddee89d34ad3567b972ef0
Milan Gurung <milan.gurungnalim@gmail.com>
2022-05-01 18:48:57 +0545

    Initial Setup | Flutter create

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:next/pages/homePage.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Next',
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
